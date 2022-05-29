6e0bd8326986d0c19ab81fad8788a9ce2b207ec9
Adiba Hasan <56604392+adibahasan11@users.noreply.github.com>
2022-05-29 19:14:36 +0600

    Print Contents

--- index.js
+++ index.js
@@ -5,14 +5,20 @@ element.onclick = async function() {
     const [currentTab] = await chrome.tabs.query({
         active: true,
         currentWindow: true,
-      });
+    });
       
-      console.log({ id: currentTab.id, url: currentTab.url });
+    console.log({ id: currentTab.id, url: currentTab.url });
 
-      newWin = window.open(currentTab.url)
-      newWin.print()
-      newWin.close()
+    newWin = window.open(currentTab.url)
+    newWin.print()
+    newWin.close()
     //   newWin.onload = function() { 
     //     console.log("Hello")
     //     newWin.print(); }
+
+    // var divToPrint = document.getElementById("floor1").innerHTML;
+    // newWin = window.open("");
+    // newWin.document.write(divToPrint);
+    // newWin.print();
+    // newWin.close();
 }
