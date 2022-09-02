4002a5a3acc2fa493ae110a9a875c7b1404a3c77
Adiba Hasan <56604392+adibahasan11@users.noreply.github.com>
2022-09-02 09:43:35 +0600

    Setting Cookies to new browser

--- api/controller/generatePDF.js
+++ api/controller/generatePDF.js
@@ -1,20 +1,28 @@
 const puppeteer = require('puppeteer')
 
 const generatePDF = async (req, res) => {
+    const browser = await puppeteer.launch({ headless: true });
+    
     console.log(req.body)
-
+    
     let url = req.body.url
     let cookie = req.body.cookies
-
-    const browser = await puppeteer.launch({ headless: true });
+    
     const page = await browser.newPage();
     
+    await page.setCookie(...req.body.cookies)
     await page.goto(url, {waitUntil: 'networkidle0'});
-    await page.setCookie(cookie)
+    console.log(page.content())
     
-    const pdf = await page.pdf({ format: 'A4' });
+    // const pdf = await page.pdf({ format: 'A4' });
+    const pdf = await page.pdf({ 
+            printBackground: true,
+            format: 'Letter'
+        }).then(
+            console.log("Done")
+        );
  
-    await browser.close();
+    // await browser.close();
 
     res.contentType("application/pdf");
     res.send(pdf);
